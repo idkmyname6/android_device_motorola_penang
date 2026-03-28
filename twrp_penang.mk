d96659c5f7d00d2113ee48d610323256d10d4d4c
idkmyname6 <aurora.cool2763@gmail.com>
2026-03-28 19:44:03 -0300

    how many more?

--- twrp_penang.mk
+++ twrp_penang.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/orangefox/config/common.mk)
 
 # Inherit from penang device
 $(call inherit-product, device/motorola/penang/device.mk)
