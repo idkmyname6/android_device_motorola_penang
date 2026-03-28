ab5f0b796ed0a1901a9a77a884c245841be0f268
idkmyname6 <aurora.cool2763@gmail.com>
2026-03-28 20:32:20 -0300

    PLEASE FOR THE LOVE OF GOD

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_penang-user
-add_lunch_combo omni_penang-userdebug
-add_lunch_combo omni_penang-eng
+add_lunch_combo twrp_penang-user
+add_lunch_combo twrp_penang-userdebug
+add_lunch_combo twrp_penang-eng
